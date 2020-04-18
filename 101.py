2af7a91e0fd70c06d9716770f001168c0d463350
Junhan0037 <wnsgks0037@naver.com>
2020-04-18 21:24:42 +0900

    Add file

--- 101.py
+++ 101.py
@@ -1,11 +1,11 @@
 # Import pandas as a alias 'pd'
-import ______ as __
+import pandas as pd
 
 # Load the CSV file "marathon_results_2017.csv" under "data" folder
-marathon_2017 = __.________("______marathon_results_2017.csv")
+marathon_2017 = pd.read_csv("./data/marathon_results_2017.csv")
 
 # Display the first five initial rows using the .head() method
-print(marathon_2017._______)
+print(marathon_2017.head())
 
 # Display data frame structure using the .info() method
-print(marathon_2017._______)
+print(marathon_2017.info())
--- 102.py
+++ 102.py
@@ -1,11 +1,11 @@
 #Checking the null values in the data fields
-marathon_2017.______().___(axis=0)
+marathon_2017.isnull().sum(axis=0)
 
 #Show columns of the data frame
-marathon_2017._______
+marathon_2017.columns
 
 #Drop some columns with null values
-marathon_2017_clean = marathon_2017.____(['Unnamed: 0','Bib','Unnamed: 9'], axis='columns')
+marathon_2017_clean = marathon_2017.drop(['Unnamed: 0','Bib','Unnamed: 9'], axis='columns')
 
 # Display the first five initial rows using the .head() method
-print(marathon_2017_clean.______)
+print(marathon_2017_clean.head())
